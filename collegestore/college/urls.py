62201d8c9a711fe23aeab7d4a052b48a1887ca80
jissurachel <jissurachel@gmail.com>
2023-11-01 13:21:34 +0530

    added

---
+++ collegestore/college/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+app_name='college'
+
+urlpatterns = [
+path('',views.home,name='home'),
+path('login', views.login,name='login'),
+path('register',views.register,name='register'),
+path('logout', views.logout,name='logout'),
+
+]
